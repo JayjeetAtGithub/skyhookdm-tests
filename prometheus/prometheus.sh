6bfa4d6e99f6c2e2a9af8920cf10b8ee86edab56
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-06-03 03:24:10 +0530

    add prometheus monitoring

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eu
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/bundle.yaml
+kubectl apply -f ./prometheus/service-monitor.yaml
+kubectl apply -f ./prometheus/prometheus.yaml
+kubectl apply -f ./prometheus/prometheus-service.yaml
+echo "http://$(kubectl -n rook-ceph -o jsonpath={.status.hostIP} get pod prometheus-rook-prometheus-0):30900"
