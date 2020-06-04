99c7e02963084788540eb68bd49f94658a9d5ff6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-06-05 02:04:24 +0530

    add steps for setting up and releasing nodes from cloudlab and automatic master selection

--- scripts/copy_config.sh
+++ scripts/copy_config.sh
@@ -12,4 +12,6 @@ echo "$ANSIBLE_SSH_KEY_DATA" | base64 -d > /root/.ssh/id_rsa
 chmod 600 /root/.ssh
 chmod 400 /root/.ssh/id_rsa
 
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$ANSIBLE_USER"@"$K8S_MASTER":/etc/kubernetes/admin.conf /workspace/kube/config
+HOSTS=$(cat /workspace/geni/hosts)
+MASTER="$(echo "$HOSTS" | cut -d' ' -f1)"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$ANSIBLE_USER"@"$MASTER":/etc/kubernetes/admin.conf /workspace/kube/config
